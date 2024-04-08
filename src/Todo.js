1e039813aabc3263ef45d5fef43c89ae78236247
csh <csh@duksung.ac.kr>
2024-04-08 13:36:18 +0900

    완료: 2024.4.3 수업

---
+++ src/Todo.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Todo = () => {
+    return(
+        <>
+        <div className="Todo">
+            <input type="checkbox" id="todo0" name="todo0" value="todo0" />
+            <label for="todo0">Todo 컴포넌트 만들기</label>
+        </div>
+        <div><h1>최승훈</h1></div>
+        </>
+    )
+}
+
+export default Todo;
