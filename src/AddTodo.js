5a7554484a15f42500e1179b096ce7d53810455f
csh <csh@duksung.ac.kr>
2024-04-10 13:15:34 +0900

    완료: 20240410 수업

---
+++ src/AddTodo.js
@@ -0,0 +1,28 @@
+import React, {useState} from "react";
+import {Button, Grid, TextField} from "@mui/material";
+
+const AddTodo = (props) => {
+    const [item, setItem] = useState({title: ""});
+
+
+    const onInputChange = (e) => {
+        setItem({title: e.target.value}); // item 상태 변수 값이 바뀌고 자동으로 리렌더링이 일어남
+        console.log(item);
+    }
+
+    return(
+        <Grid container style={{marginTop:20}}>
+            <Grid xs={11} md={11} item style={{paddingRight:16}}>
+                <TextField placeholder="Add Todo here" fullWidth 
+                onChange={onInputChange} value={item.title} />
+            </Grid>
+            <Grid xs={1} md={1} item>
+                <Button fullWidth style={{height:'100%'}} color="secondary" variant="outlined">
+                    +
+                </Button>
+            </Grid>
+        </Grid>
+    )
+}
+
+export default AddTodo;
--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
 import './App.css';
 import Todo from './Todo';
+import React, {useState} from "react";
+import { Container, List, Paper } from "@mui/material";
+import AddTodo from './AddTodo';
 
 
 function App() {
+  const [items, setItems] = useState([{
+    id: "0",
+    title: "Hello World 1",
+    done: true
+  }, {
+    id: "1",
+    title: "Hello World 2",
+    done: false
+  }
+  ]);
+
+  const addItem = (item) => {
+    item.id = "ID-" + items.length;
+    item.done = false;
+    setItems([...items, item]); // items 배열에 item 원소 추가
+    console.log("items: ", items);
+  }
+
+  // JSX 결과를 변수에 저장
+  let todoItems = 
+    items.length > 0 && (
+      <Paper style={{margin: 16}}>
+        <List>
+          {items.map((item)=> <Todo item={item} key={item.id} />)}; 
+        </List>
+      </Paper>
+    )
+
+
+
+  // 변수를 반환
   return (
     <div className="App">
-      <Todo />
-      <Todo />
-      <Todo />
+      <Container maxWidth="md">
+        <AddTodo />
+        <div className="App">
+          {todoItems}
+        </div>
+      </Container>
     </div>
   );
 }
