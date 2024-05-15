45d59192e1364e75301d81129bf51c5594b4e1bb
csh <csh@duksung.ac.kr>
2024-05-15 13:08:04 +0900

    완료: 20240515(5.5절 완성)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import './App.css';
 import Todo from './Todo';
 import React, {useState, useEffect } from "react";
-import { Container, List, Paper } from "@mui/material";
+import { Container, List, Paper, Grid, Button, AppBar, Toolbar, Typography } from "@mui/material";
 import AddTodo from './AddTodo';
-import { call } from "./service/ApiService";
+import { call, signout } from "./service/ApiService";
 
 
 function App() {
   const [items, setItems] = useState([]);
-
+  const [loading, setLoading] = useState(true);
 
 
   useEffect( () => {
     call("/todo", "GET", null)
-      .then( (response) => setItems(response.data));
+      .then( (response) => {
+        setItems(response.data);
+        setLoading(false);
+      });
   }, [] );
 
 
@@ -44,16 +47,47 @@ function App() {
       </Paper>
     )
 
+  let navigationBar = (
+    <AppBar position="static">
+      <Toolbar>
+        <Grid justifyContent="space-between" container>
+          <Grid item>
+            <Typography variant='h6'>오늘의 할일</Typography>
+          </Grid>
+          <Grid item>
+            <Button color="inherit" raised onClick={signout}>
+              로그아웃
+            </Button>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
 
-  // 변수를 반환
-  return (
-    <div className="App">
+  let todoListPage = (
+    <div>
+      {navigationBar}
       <Container maxWidth="md">
         <AddTodo addItem={addItem} />
         <div className="App">
           {todoItems}
         </div>
       </Container>
+    </div>
+  );
+
+  let loadingPage = <h1>로딩 중...</h1>
+
+  let content = loadingPage;
+
+  if(!loading) {
+    content = todoListPage;
+  }
+
+  // 변수를 반환
+  return (
+    <div className="App">
+      {content}
     </div>
   );
 }
--- src/SignUp.js
+++ src/SignUp.js
@@ -1,66 +1,77 @@
 import React from "react";
 import { Container, Grid, Typography, TextField, Button } from "@mui/material";
-import { signin } from "./service/ApiService";
-import {Link } from "react-router-dom";
+import { signup } from "./service/ApiService";
+import { Link } from "react-router-dom";
 
-const Login = () => {
+function SignUp() {
     const handleSubmit = (event) => {
         event.preventDefault();
+
         const data = new FormData(event.target);
         const username = data.get("username");
         const password = data.get("password");
 
-        signin({username: username, password: password});
+        signup({username: username, password: password}).then(
+            (response) => {
+                window.location.href = "/login";
+            }
+        );
     };
 
     return(
         <Container component="main" maxWidth="xs" style={{marginTop: "8%"}}>
-            <Grid container spacing={2}>
-                <Grid item xs={12}>
-                    <Typography component="h1" variant="h5">
-                        로그인
-                    </Typography>
-                </Grid>
-            </Grid>
             <form noValidate onSubmit={handleSubmit}>
-                {" "}
                 <Grid container spacing={2}>
                     <Grid item xs={12}>
+                        <Typography component="h1" variant="h5">
+                            계정 생성
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12}>
                         <TextField
+                            autoComplete="fname"
+                            name="username"
                             variant="outlined"
                             required
                             fullWidth
                             id="username"
                             label="아이디"
-                            name="username"
-                            autoComplete="username"
+                            autoFocus
                         />
                     </Grid>
                     <Grid item xs={12}>
-                        <TextField
+                        <TextField                            
+                            name="password"
                             variant="outlined"
                             required
                             fullWidth
                             id="password"
                             label="패스워드"
-                            name="password"
+                            autoFocus
                             autoComplete="current-password"
                         />
                     </Grid>
                     <Grid item xs={12}>
-                        <Button type="submit" fullWidth variant="contained" color="primary">
-                            로그인
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                        >
+                            계정 생성
                         </Button>
                     </Grid>
+                </Grid>
+                <Grid container justify="flex-end">
                     <Grid item>
-                        <Link to="/signup" variant="body2">
-                            계정이 없습니까? 여기서 가입하세요.
+                        <Link to="/login" variant="body2">
+                            이미 계정이 있습니까? 로그인하세요.
                         </Link>
                     </Grid>
                 </Grid>
             </form>
         </Container>
-    );
-};
+    )
+}
 
-export default Login;
+export default SignUp;
